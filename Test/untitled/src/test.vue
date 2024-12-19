872769fa7f6bd6d40bfae4415a11039eaf2b112c
Vincent2024H95 <118534454+Vincent2024H95@users.noreply.github.com>
2024-12-19 13:28:05 +0800

    no message

---
+++ Test/untitled/src/test.vue
@@ -0,0 +1,14 @@
+import { createApp, ref } from 'vue'
+
+createApp({
+    setup(){
+      return {
+        count:ref(0)
+  }
+    }
+      }).mount('#app')
+<div id="app">
+  <button @click="count++">
+    Count is:{{ count }}
+  </button/>
+</div>
